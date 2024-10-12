fbd0529b9d131fa199715378466acebaa428cb05
Aakansha Agarwal <aakansha.agarwal.01@gmail.com>
2024-10-12 22:52:40 +0800

    Create os_tasks.py

---
+++ os_tasks.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+import os
+
+userlist = ["alpha", "beta", "gamma"]
+
+print("Adding users to system")
+print("######################################################")
+
+# loop to add users from userslist
+for user in userlist:
+    exitcode = os.system("id {}".format(user) )
+    if exitcode != 0:
+        print("User {} does not exist. Adding it.".format(user))
+        print("************************************************")
+        print()
+        os.system("useradd {}".format(user))
+    else:
+        print("User already exist. skipping it.")
+        print("************************************************")
+        print()
+
+# Condition to check if a group exists or not. add if does not exists
+exitcode = os.system("grep science /etc/group")
+if exitcode != 0:
+    print("Group science does not exist. Adding it.")
+    print("******************************************")
+    print()
+    os.system("groupadd science")
+else:
+    print("Group already exist, skipping it.")
+    print("******************************************")
+    print()
+
+for user in userlist:
+    print("Adding user {} in the science group".format(user))
+    print("******************************************")
+    print()
+    os.system("usermod -G science {}".format(user))
+
+print("Adding Directory")
+print("######################################################")
+print()
+
+if os.path.isdir("/opt/science_dir"):
+    print("Directory already exists, skipping it")
+else:
+    os.mkdir("/opt/science_dir")
+
+print("Assigning permission and ownership to the directory")
+print("######################################################")
+print()
+os.system("chown :science /opt/science_dir")
+os.system("chmod 770 /opt/science_dir")
